6f80c08306713e66d521f11c5a5261997dcb9774
huxg <997927265@qq.com>
2019-11-08 11:59:38 +0800

    菜园

--- pages/garden/garden.js
+++ pages/garden/garden.js
@@ -6,26 +6,69 @@ Page({
     activeIndex: 0,
     sliderOffset: 0,
     sliderLeft: 0,
+    
+    showInput: false,
+    inputVal: '',
+    top: '',
+    sendcolor: false,//发送按钮颜色
     //社区列表
-    DataSource: [1, 1, 1, 1, 1],
-    icon: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3175633703,3855171020&fm=27&gp=0.jpg',
-    content: '一分钟了解阳台种菜一分钟了解阳台种菜，一分钟了解阳台种菜',
-    resource: ['http://img2.imgtn.bdimg.com/it/u=2118739199,3378602431&fm=27&gp=0.jpg',
-      'http://img0.imgtn.bdimg.com/it/u=2277942808,1417432970&fm=27&gp=0.jpg',
-      'http://img5.imgtn.bdimg.com/it/u=1504812505,3480403568&fm=27&gp=0.jpg',
-      'http://img4.imgtn.bdimg.com/it/u=3456219059,4251129897&fm=27&gp=0.jpg',
-      'http://img3.imgtn.bdimg.com/it/u=3912316188,1981132393&fm=27&gp=0.jpg'
+    dataList: [
+      {
+        status:'1',
+        title:'自己阳台种的青菜',
+        uploader:'王珊珊',
+        time:'2019.06.22',
+        phone:'18578651610',
+        address:'宝钢小区',
+        content: '有的青菜大概30斤左右，可以菜品互换，也可以零售（100/斤），仅限附近3公里交换，有需要的朋友请联系我，谢谢！',
+        showBtn:false,
+        resource: ['http://img2.imgtn.bdimg.com/it/u=2118739199,3378602431&fm=27&gp=0.jpg',
+            'http://img0.imgtn.bdimg.com/it/u=2277942808,1417432970&fm=27&gp=0.jpg',
+            'http://img5.imgtn.bdimg.com/it/u=1504812505,3480403568&fm=27&gp=0.jpg',
+            'http://img4.imgtn.bdimg.com/it/u=3456219059,4251129897&fm=27&gp=0.jpg',
+            'http://img3.imgtn.bdimg.com/it/u=3912316188,1981132393&fm=27&gp=0.jpg'
+          ],
+          zanSource: ['张三', '李四', '王五', '找钱', '孙俪', '王宝'],
+          contnet: [{
+            'firstname': '张三',
+            'content': '你好漂亮呀！！'
+          },
+          {
+            'firstname': '李四',
+            'content': '纳尼！！'
+          },
+          
+        ],
+      },
+      {
+        status: '0',
+        title: '自己阳台种的青菜',
+        uploader: '王珊珊',
+        time: '2019.06.22',
+        phone: '18578651610',
+        address: '宝钢小区',
+        content: '有的青菜大概30斤左右，可以菜品互换，也可以零售（100/斤），仅限附近3公里交换，有需要的朋友请联系我，谢谢！',
+        showBtn: false,
+        resource: ['http://img2.imgtn.bdimg.com/it/u=2118739199,3378602431&fm=27&gp=0.jpg',
+          'http://img0.imgtn.bdimg.com/it/u=2277942808,1417432970&fm=27&gp=0.jpg',
+          'http://img5.imgtn.bdimg.com/it/u=1504812505,3480403568&fm=27&gp=0.jpg',
+          'http://img4.imgtn.bdimg.com/it/u=3456219059,4251129897&fm=27&gp=0.jpg',
+          'http://img3.imgtn.bdimg.com/it/u=3912316188,1981132393&fm=27&gp=0.jpg'
+        ],
+        zanSource: ['张三', '李四', '王五', '找钱', '孙俪', '王宝'],
+        contnet: [{
+          'firstname': '张三',
+          'content': '你好漂亮呀！！'
+        },
+        {
+          'firstname': '李四',
+          'content': '纳尼！！'
+        },
+
+        ],
+      }
     ],
-    zanSource: ['张三', '李四', '王五', '找钱', '孙俪', '王宝'],
-    contnet: [{
-      'firstname': '张三',
-      'content': '你好漂亮呀！！'
-    },
-    {
-      'firstname': '李四',
-      'content': '纳尼！！'
-    }
-    ],
+   
     photoWidth: wx.getSystemInfoSync().windowWidth / 5,
 
     popTop: 0, //弹出点赞评论框的位置
@@ -42,6 +85,69 @@ Page({
         });
       }
     });
+  },
+  operating(e) {
+    // console.log(e.currentTarget.dataset.bindex);
+    var index = e.currentTarget.dataset.bindex;
+    if (this.data.dataList[index].showBtn) {
+      this.data.dataList[index].showBtn = false;
+    } else {
+      this.data.dataList[index].showBtn = true;
+    }
+    // console.log(this.data.dataList); 
+    this.setData({
+      dataList: this.data.dataList
+    })
+  },
+  //点赞+1
+  like() {
+    console.log('点赞+1');
+  },
+  //评价
+  discuss(e) {
+    var index = e.currentTarget.dataset.bindex;
+    console.log(e)
+    var top = e.detail.x;//获取键盘高度定位
+    console.log(top)
+    if (this.data. dataList[index].showBtn) {
+      this.data. dataList[index].showBtn = false;
+    } else {
+      this.data. dataList[index].showBtn = true;
+    }
+    this.setData({
+      top: top + 'px',
+      showInput: true,
+       dataList: this.data. dataList
+    })
+  },
+  //输入
+  bindinput(e) {
+    console.log(e.detail.value);
+    if (e.detail.value.length > 0) {
+      this.setData({
+        inputVal: e.detail.value,
+        sendcolor: true
+      });
+    } else {
+      this.setData({
+        sendcolor: false
+      });
+    }
+
+  },
+  send() {
+    var inputVal = this.data.inputVal;
+    console.log(inputVal);
+    this.setData({
+      showInput: false
+    })
+  },
+  inputBlur() {
+    console.log(11111)
+    this.setData({
+      showInput: false,
+      top: '-200px'
+    })
   },
   // 去发布
   toadd() {
